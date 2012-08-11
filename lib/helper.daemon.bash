4bcfc1d431624377392be2b8157468135e81f09e
Felix C. Stegerman <flx@obfusk.net>
2012-08-11 16:04:16 +0200

    daemon: + chk/status, improved start/stop
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- lib/helper.daemon.bash
+++ lib/helper.daemon.bash
@@ -11,6 +11,15 @@
 #
 # --                                                            # }}}1
 
+# TODO {
+#
+#   * deal w/ old pidfile lingering after reboot !!!
+#     --> init script should clean them all !?
+#
+# } TODO
+
+# --
+
 if [ "$nap_helper_daemon_loaded" == yes ]; then
   return
 else
@@ -39,20 +48,37 @@ function nap_helper_daemon_nginx () {                           # {{{1
     "$nap_helper_daemon_nginx_conf"
 }                                                               # }}}1
 
-# Usage: nap_helper_daemon_alive <pid>
-# Checks if pid is alive; returns non-zero if not.
-function nap_helper_daemon_alive () { kill -0 "$1" 2>/dev/null; }
+# Usage: nap_helper_daemon_getpid
+# Outputs pid from pidfile (if any).
+function nap_helper_daemon_getpid () {                          # {{{1
+  [ -f "$nap_app_pidfile" ] && cat "$nap_app_pidfile"
+}                                                               # }}}1
 
-# Usage: nap_helper_daemon_chk_ok <n> <pid>
+# Usage: nap_helper_daemon_chk <pid> [<name>]
+# Checks if pid is alive; warns if name not equal to command; returns
+# non-zero if not alive.
+function nap_helper_daemon_chk () {                             # {{{1
+  local pid="$1" name="$2"
+  local cmd="$( ps -p "$pid" -o comm= )"
+
+  [ -z "$cmd" ] && return 1
+
+  if [ -n "$name" -a "$name" != "$cmd" ]; then
+    opoo "process with PID $pid has command \`$cmd';"
+    opoo "was expecting \`$name'"
+  fi
+}                                                               # }}}1
+
+# Usage: nap_helper_daemon_chk_wait <n> <pid>
 # Waits n secs to see if process dies; dies on failure.
-function nap_helper_daemon_chk_ok () {                          # {{{1
+function nap_helper_daemon_chk_wait () {                        # {{{1
   local n="$1" pid="$2" i
 
   if [ "$n" -gt 0 ]; then
     ohai "[wait $n seconds]"
     for (( i=0 ; i < n ; ++i )); do sleep 1; echo -n .; done
   fi
-  if ! nap_helper_daemon_alive "$pid"; then
+  if ! nap_helper_daemon_chk "$pid"; then
     [ "$n" -gt 0 ] && echo; die 'process died'
   fi
   [ "$n" -gt 0 ] && echo ' OK'
@@ -71,33 +97,61 @@ function nap_helper_daemon_deps () {                            # {{{1
   dpop
 }                                                               # }}}1
 
-# Usage: nap_helper_daemon_start <n> <cmd> <arg(s)>
-# Starts daemon; runs nap_helper_daemon_chk_ok; dies on failure.
+# Usage: nap_helper_daemon_status [<name>]
+# Uses pidfile and nap_helper_daemon_chk to see if process is running;
+# outputs "stopped" if stopped, "<pid>" when running.
+function nap_helper_daemon_status () {                          # {{{1
+  local name="$1"
+  local pid="$( nap_helper_daemon_getpid )"
+
+  if [ -n "$pid" ] && nap_helper_daemon_chk "$pid" ${name:+"$name"}
+  then
+    echo "$pid"
+  else
+    echo stopped
+  fi
+}                                                               # }}}1
+
+# Usage: nap_helper_daemon_start <n> [nohup] <cmd> <arg(s)>
+# Starts daemon (if not running); runs nap_helper_daemon_chk_wait;
+# dies on failure.
 function nap_helper_daemon_start () {                           # {{{1
-  local n="$1"; shift
+  local n="$1" nohup="$2"; shift
+  if [ "$nohup" == nohup ]; then shift; else nohup= ; fi
 
-  ohai "$( join ' ' "$@" )"
-  dpush "$nap_app_app"
-    "$@"  >> "$nap_app_log/deamon.log" \
-         2>> "$nap_app_log/deamon-error.log" &
-    local pid=$!
-  dpop
+  local info="$( join ' ' "$@" )"
+  local name="${info%% *}"
+  local status="$( nap_helper_daemon_status "$name" )"
 
-  nap_helper_daemon_chk_ok "$pid"
-  try '[mkpid] failed' nap_mkpid "$nap_app_pidfile" "$pid"
+  if [[ "$status" != stopped ]]; then
+    opoo "[is running] $name"
+  else
+    ohai "$info"
+    dpush "$nap_app_app"
+      $nohup "$@"  >> "$nap_app_log/deamon.log" \
+                  2>> "$nap_app_log/deamon-error.log" &
+      local pid=$!
+    dpop
+
+    nap_helper_daemon_chk_wait "$pid"
+    try '[mkpid] failed' nap_mkpid "$nap_app_pidfile" "$pid"
+  fi
 }                                                               # }}}1
 
 # Usage: nap_helper_daemon_stop <info>
-# Stops deamon by killing pid; dies on failure.
+# Stops deamon (if running) by killing pid; dies on failure.
 function nap_helper_daemon_stop () {                            # {{{1
   local info="$1"
+  local name="${info%% *}"
+  local status="$( nap_helper_daemon_status "$name" )"
 
-  pid="$( cat "$nap_app_pidfile" )"
-  [ -n "$pid" ] || die '[getpid] failed'
-
-  ohai "[kill] $info"
-  try '[kill] failed' kill "$pid"
-  try '[rmpid] failed' rm "$nap_app_pidfile"
+  if [[ "$status" == stopped ]]; then
+    opoo "[not running] $name"
+  else
+    ohai "[kill] $name"
+    try '[kill] failed' kill "$status"    # $status is pid if running
+    try '[rmpid] failed' rm "$nap_app_pidfile"
+  fi
 }                                                               # }}}1
 
 # --
